048552bcbfac3c25e62d4c74e20edaa74b293f15
Junseo Yoon <58902513+liljuice@users.noreply.github.com>
2025-06-25 17:31:56 +0900

    feat: 새로운 Window의 초기 디자인 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,6 @@ const Home = () => {
   const [questions, setQuestions] = useState([]);
 
   useEffect(() => {
-    // 기존 fetchTodayQuestion 대신 fetchRecommendQuestion을 호출합니다.
     fetchRecommendQuestion().then((data) => setRecommendedQuestion(data));
     fetchQuestions(1).then((data) => setQuestions(data.content));
   }, []);
@@ -19,22 +18,23 @@ const Home = () => {
   return (
     <div className="flex flex-col justify-center bg-gradient-to-br from-background-base to-white/10">
       {/* gradient */}
-      <div className="w-full py-50 flex flex-col justify-center items-center">
+      {/* Window와 하단 질문 리스트의 가로 폭을 맞추기 위해 px-120을 추가합니다. */}
+      <div className="w-full py-50 flex flex-col items-center px-120">
         <Window todayQuestion={recommendedQuestion} />
 
         {/* 추천 문제가 있을 경우에만 답변 작성하기 버튼을 보여주고, 해당 문제 상세 페이지로 연결합니다. */}
-          {recommendedQuestion && (
-            <Link
-              to={`/questions/detail/${recommendedQuestion.id}`}
-              className="items-center px-50 py-16 text-lg rounded-[10px] text-white gradient-blue"
-            >
-              답변 작성하기
-            </Link>
-         )}
+        {recommendedQuestion && (
+          <Link
+            to={`/questions/detail/${recommendedQuestion.id}`}
+            className="items-center px-50 py-16 text-lg rounded-[10px] text-white gradient-blue"
+          >
+            답변 작성하기
+          </Link>
+        )}
       </div>
 
       <p className="text-gray-500 text-center py-24">
-        매일 새로운 문제가 생성됩니다.
+        매일 카테고리 별로 새로운 문제가 생성됩니다.
       </p>
 
       {/* CS 면접 질문 리스트 */}
