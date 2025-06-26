00c633eb6484024c37a343a85086739916e04ea3
Junseo Yoon <58902513+liljuice@users.noreply.github.com>
2025-06-26 11:46:46 +0900

    fix: 질문 mock 데이터 수정

--- src/mocks/homeData.js
+++ src/mocks/homeData.js
@@ -1,7 +1,7 @@
 export const mockRecommendedQuestion = {
-  id: 101,
+  id: 5,
   category: "네트워크",
-  content: "TCP/IP의 개념을 설명하세요.",
+  content: "TCP/IP의 개념을 설명하세요",
   // Window 컴포넌트에서 사용하는 isSubmitted 속성을 추가합니다.
   isSubmitted: false,
 };
@@ -13,34 +13,48 @@ export const mockQuestions = [
     content: "배열과 연결 리스트의 차이점은 무엇인가요?",
     // CSQuestionTable에서 사용하는 createdAt, isSubmitted 속성을 추가합니다.
     createdAt: "2025-06-25T12:34:56",
-    isSubmitted: true,
+    isSubmitted: false,
   },
   {
     id: 2,
     category: "운영체제",
-    content: "프로세스와 스레드의 차이점에 대해 설명해주세요.",
+    content: "프로세스와 스레드의 차이점을 설명하세요",
     createdAt: "2025-06-25T12:34:56",
-    isSubmitted: false,
+    isSubmitted: true,
   },
   {
     id: 3,
     category: "네트워크",
     content: "HTTP와 HTTPS의 차이점은 무엇인가요?",
-    createdAt: "2025-06-25T12:34:56",
-    isSubmitted: true,
+    createdAt: "2025-06-24T12:34:56",
+    isSubmitted: false,
   },
     {
     id: 4,
     category: "알고리즘",
     content: "퀵 정렬의 시간 복잡도는 어떻게 되나요?",
-    createdAt: "2025-06-25T12:34:56",
-    isSubmitted: true,
+    createdAt: "2025-06-24T12:34:56",
+    isSubmitted: false,
   },
       {
     id: 5,
     category: "네트워크",
-    content: "TCP/IP의 개념을 설명하세요.",
-    createdAt: "2025-06-25T12:34:56",
-    isSubmitted: false,
+    content: "TCP/IP의 개념을 설명하세요",
+    createdAt: "2025-06-23T12:34:56",
+    isSubmitted: true,
+  },
+        {
+    id: 6,
+    category: "알고리즘",
+    content: "이진탐색을 설명하세요",
+    createdAt: "2025-06-23T12:34:56",
+    isSubmitted: true,
+  },
+          {
+    id: 7,
+    category: "운영체제",
+    content: "Context Switch에 대해 설명하세요",
+    createdAt: "2025-06-22T12:34:56",
+    isSubmitted: true,
   },
 ];
